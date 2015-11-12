ef01150403c7eaf4fb8e1fa1c9cf96248364fc57
Maitê Balhester <mbalhest@thoughtworks.com>
2015-11-12 15:54:03 -0200

    Add acceptance tests for puppet-haveged
    This patch adds some acceptance tests for puppet-haveged, making sure
    that we can applied it without errors and ensuring idempotency.
    Also we check if the packages are installed and the services are
    running.
    
    Change-Id: Ieab5554ddf45c0311d6de7e197c856c87cbccd92

---
+++ spec/acceptance/basic_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper_acceptance'
+
+describe 'puppet-apt_mirror module' do
+  def pp_path
+    base_path = File.dirname(__FILE__)
+    File.join(base_path, 'fixtures')
+  end
+
+  def default_puppet_module
+    module_path = File.join(pp_path, 'default.pp')
+    File.read(module_path)
+  end
+
+  it 'should work with no errors' do
+    apply_manifest(default_puppet_module, catch_failures: true)
+  end
+
+  it 'should be idempotent' do
+    apply_manifest(default_puppet_module, catch_changes: true)
+  end
+
+  describe package('haveged') do
+    it { should be_installed }
+  end
+
+  describe service('haveged') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
